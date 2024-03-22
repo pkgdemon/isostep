9397c5ee69100bdcc0e3ea173eb62952e8e02438
pkgdemon <pkgdemonteam@gmail.com>
2024-03-21 23:11:10 -0400

    Removing installer for now

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf live-default
 mkdir live-default
 cd live-default || exit
-lb config --distribution bookworm --debian-installer live --archive-areas "main contrib non-free non-free-firmware" --iso-volume "Debian Live"
+lb config --distribution bookworm --archive-areas "main contrib non-free non-free-firmware" --iso-volume "Debian Live"
 
 cat <<EOF > config/hooks/live/gnustep.hook.chroot
 #!/bin/sh
