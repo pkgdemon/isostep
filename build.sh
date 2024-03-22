41c7f30d0025014bb89a254d8b374e24005a0d2a
pkgdemon <pkgdemonteam@gmail.com>
2024-03-22 19:02:04 -0400

    Change when package is installed

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ mkdir live-default
 cd live-default || exit
 lb config --distribution bookworm --archive-areas "main contrib non-free non-free-firmware" --iso-volume "GNUstep Live"
 
+echo "sddm" > config/package-lists/gnustep.list.chroot
+
 mkdir -p config/includes.chroot_after_packages/usr
 cp -R ../usr/* config/includes.chroot_after_packages/usr
 mkdir -p config/includes.chroot_after_packages/etc
@@ -16,7 +18,6 @@ cat <<EOF > config/hooks/live/gnustep.hook.chroot
 set -e
 
 apt install -y git
-apt install -y sddm
 
 git clone https://github.com/pkgdemon/debstep
 cd /debstep && ./debstep-installer
