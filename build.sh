10a3c0b595feb0ed3166f04415fab4bb47d9f326
pkgdemon <pkgdemonteam@gmail.com>
2024-03-21 22:00:12 -0400

    Add initial build script for ISO

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf live-default
+mkdir live-default
+cd live-default
+lb config --distribution bookworm --debian-installer live --archive-areas "main contrib non-free non-free-firmware" --iso-volume "Debian Live"
+
+cat <<EOF > config/hooks/live/gnustep.hook.chroot
+#!/bin/sh
+
+set -e
+
+apt install -y git
+
+git clone https://github.com/pkgdemon/debstep
+cd /debstep && ./debstep-installer
+rm -rf /debstep
+rm -rf /gnustep-src
+EOF
+
+lb build
