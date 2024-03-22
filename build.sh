04c2c24549449c30fb59320815b9d006b6fe67d1
pkgdemon <pkgdemonteam@gmail.com>
2024-03-21 22:38:14 -0400

    Exit if cd fails

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf live-default
 mkdir live-default
-cd live-default
+cd live-default || exit
 lb config --distribution bookworm --debian-installer live --archive-areas "main contrib non-free non-free-firmware" --iso-volume "Debian Live"
 
 cat <<EOF > config/hooks/live/gnustep.hook.chroot
